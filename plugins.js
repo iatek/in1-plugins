acb1437ed0910bb732d8bfd6d33812efd7044b1b
Carol Skelly <carol@iatek.com>
2012-07-31 07:05:23 -0400

    new

--- plugins.js
+++ plugins.js
@@ -1,4 +1,4 @@
 
 exports.socialist = function(req, res){
-    res.render('socialist/index',{ title:'jQuery Socialist - Social Media Plugin - Create and combine feeds from social networks', keywords:'jquery social plugin,jquery facebook,social plugins,jquery twitter feed'});
+    res.render('socialist/index',{ layout:true, title:'jQuery Socialist - Social Media Plugin - Create and combine feeds from social networks',keywords:'jquery social plugin,jquery facebook,social plugins,jquery twitter feed'});
 }
