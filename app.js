47fb3091ee30a05954c44c6c7f5cc4a26a4d69a5
Carol Skelly <carol@iatek.com>
2012-07-31 07:07:45 -0400

    new

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var app = express.createServer(
 app.configure( function () {
     app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
+    app.set('view options', { layout: true }); 
 	app.use("/public", express.static(__dirname + '/public'));
 	app.use("/styles", express.static(__dirname + '/styles'));
 	app.use("/scripts", express.static(__dirname + '/scripts'));
