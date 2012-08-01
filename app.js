4278c1fdcdca84915e06c61345359229e723a824
Carol Skelly <carol@iatek.com>
2012-08-01 04:16:21 -0400

    df

--- app.js
+++ app.js
@@ -7,24 +7,17 @@ var app = express.createServer(
 );
 
 app.configure( function () {
-    //app.use(app.router);
     app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
-    app.set('view options', { layout:'layout9.ejs' }); 
-    /*
 	app.use("/public", express.static(__dirname + '/public'));
 	app.use("/styles", express.static(__dirname + '/styles'));
 	app.use("/scripts", express.static(__dirname + '/scripts'));
 	app.use("/images", express.static(__dirname + '/images'));
-    */
+    app.use(app.router);
 });
 
 app.get('/', function (req, res) {
 	res.render('index');
-});
-
-app.get('/l', function (req, res) {
-	res.render('layout',{body:'sdasdfsdf'});
 });
 
 app.get('/socialist', plugins.socialist);
