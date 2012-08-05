652610bfef54770c9c2902b66d22d4286a40405e
Carol Skelly <carol@iatek.com>
2012-08-05 14:10:55 -0400

    layout updates

--- plugins.js
+++ plugins.js
@@ -1,14 +1,26 @@
 
 exports.socialist = function(req, res){
-    res.render('socialist/index',{ title:'jQuery Socialist Social Media Plugin - Create and combine feeds from social networks', keywords:'jquery social plugin,jquery facebook,social plugins,jquery twitter feed'});
+    res.render('socialist/index',{
+        title:'jQuery Socialist Social Media Plugin - Create and combine feeds from social networks',
+        keywords:'jquery social plugin,combine social feeds,social feed plugin,jquery facebook,social media plugin,jquery twitter feed'
+        }
+    );
 }
 
 exports.socialist.demo = function(req, res){
-    res.render('socialist/demo',{ title:'jQuery Socialist Social Media Plugin - Create and combine feeds from social networks', keywords:'jquery social plugin,jquery facebook,social plugins,jquery twitter feed'});
+    res.render('socialist/demo',{
+        title:'jQuery Socialist Social Media Plugin - Combine social feeds from multiple social networks',
+        keywords:'jquery social plugin,combine social feeds,social feed plugin,jquery facebook,social media plugin,jquery twitter feed,pinterest plugin,craigslist,combine facebook and twitter,linkedin'
+        }
+    );
 }
 
 exports.share = function(req, res){
-    res.render('share/index',{ title:'jQuery Share Social Media Plugin - Simple social media buttons for sharing to multiple social networks', keywords:'jquery social plugin,jquery,share to facebook,social plugin,sharing plugin,sharing tool,jquery twitter post,tweet post'});
+    res.render('share/index',{
+        title:'jQuery Share Social Media Plugin - Simple social media buttons for sharing to multiple social networks',
+        keywords:'jquery social plugin,sharing buttons,share,sharing plugin,multiple post sharing,jquery facebook,jquery post tweet'
+        }
+    );
 }
 
 exports.switchr = function(req, res){
